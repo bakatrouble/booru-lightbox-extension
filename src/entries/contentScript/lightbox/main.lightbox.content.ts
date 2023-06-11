64b1a3a2909c738f1848e8a8229952f736771983
bakatrouble <bakatrouble@gmail.com>
2023-06-11 23:52:26 +0300

    🐛 preserve vuetify styles already present on page

--- src/entries/contentScript/lightbox/main.lightbox.content.ts
+++ src/entries/contentScript/lightbox/main.lightbox.content.ts
@@ -19,20 +19,28 @@ if (collectImagesModule) {
         },
         theme: {
             defaultTheme: 'dark',
+            cspNonce: 'lightbox',
         },
     });
     renderContent(
         'lightbox',
         import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS,
         (appRoot: HTMLElement) => {
+            const elementToBackup = document.querySelector('#vuetify-theme-stylesheet');
+            if (elementToBackup) {
+                elementToBackup.id = 'vuetify-theme-stylesheet-backup';
+            }
             app = createApp(Primary, { collectImagesModule })
                 .use(vuetify)
                 .use(PortalVue);
             app.mount(appRoot);
-            const themeStylesheet = document.querySelector('#vuetify-theme-stylesheet') as HTMLLinkElement;
+            const themeStylesheet = document.querySelector('#vuetify-theme-stylesheet[nonce=lightbox]') as HTMLLinkElement;
             themeStylesheet.id = 'vuetify-theme-stylesheet-lightbox';
             appRoot.appendChild(themeStylesheet.cloneNode(true));
             themeStylesheet.remove();
+            if (elementToBackup) {
+                elementToBackup.id = 'vuetify-theme-stylesheet';
+            }
         },
     );
 }
--- src/entries/contentScript/notifications/main.notifications.content.ts
+++ src/entries/contentScript/notifications/main.notifications.content.ts
@@ -4,6 +4,7 @@ import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 import Notifications from './Notifications.vue';
 import renderContent from '~/entries/contentScript/renderContent';
 import 'vuetify/lib/styles/main.sass';
+import { createHead } from '@vueuse/head';
 
 const vuetify = createVuetify({
     icons: {
@@ -13,19 +14,27 @@ const vuetify = createVuetify({
     },
     theme: {
         defaultTheme: 'dark',
+        cspNonce: 'notifications',
     },
 });
 renderContent(
     'notifications',
     import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS,
     (appRoot: HTMLElement) => {
+        const elementToBackup = document.querySelector('#vuetify-theme-stylesheet');
+        if (elementToBackup) {
+            elementToBackup.id = 'vuetify-theme-stylesheet-backup';
+        }
         createApp(Notifications)
             .use(vuetify)
             .mount(appRoot);
-        const themeStylesheet = document.querySelector('#vuetify-theme-stylesheet') as HTMLLinkElement;
+        const themeStylesheet = document.querySelector('#vuetify-theme-stylesheet[nonce=notifications]') as HTMLLinkElement;
         themeStylesheet.id = 'vuetify-theme-stylesheet-notifications';
         appRoot.appendChild(themeStylesheet.cloneNode(true));
         themeStylesheet.remove();
+        if (elementToBackup) {
+            elementToBackup.id = 'vuetify-theme-stylesheet';
+        }
     },
 ).then(() => {
 })
