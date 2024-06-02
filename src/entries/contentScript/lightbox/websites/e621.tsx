63de802d499f8df9f6f91b72f3281c25bcebf9a0
bakatrouble <bakatrouble@gmail.com>
2024-06-02 17:48:50 +0300

    🐛 Show placeholder for e621 swfs

--- src/entries/contentScript/lightbox/websites/e621.tsx
+++ src/entries/contentScript/lightbox/websites/e621.tsx
@@ -2,14 +2,39 @@ import { CollectImagesOptions, MediaListItem, MediaType } from '~/entries/conten
 
 const e621CollectImages: CollectImagesOptions = {
     callback: async (): Promise<MediaListItem[]> =>
-        Array.from(document.querySelectorAll('.post-preview')).map(el => ({
-            el: el as HTMLElement,
-            item: {
-                src: el.getAttribute('data-file-url')!,
-                label: <a href={el.querySelector('a')!.href} target="_blank">Show image</a>,
-                type: el.getAttribute('data-file-ext') === 'webm' ? MediaType.Video : MediaType.Image,
-            },
-        })),
+        Array.from(document.querySelectorAll('.post-preview')).map(elem => {
+            const el = elem as HTMLElement;
+            const fileUrl = el.getAttribute('data-file-url')!;
+            const fileExt = el.getAttribute('data-file-ext');
+            let item: MediaListItem["item"];
+            switch (fileExt) {
+            case 'swf':
+                item = {
+                    src: el.querySelector('img')!.src,
+                    label: <a href={el.querySelector('a')!.href} target="_blank">Show flash</a>,
+                    type: MediaType.Image,
+                };
+                break;
+            case 'webm':
+                item = {
+                    src: fileUrl,
+                    label: <a href={el.querySelector('a')!.href} target="_blank">Show video</a>,
+                    type: MediaType.Video,
+                };
+                break;
+            default:
+                item = {
+                    src: fileUrl,
+                    label: <a href={el.querySelector('a')!.href} target="_blank">Show image</a>,
+                    type: MediaType.Image,
+                };
+                break;
+            }
+            return ({
+                el,
+                item,
+            })
+        }),
     domains: ['e621.net', 'e926.net'],
 };
 export default e621CollectImages;
